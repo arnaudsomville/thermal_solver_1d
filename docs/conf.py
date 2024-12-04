4dcbb58bcb33565c260c99d8cd6735c7b7ba301e
Arnaud Somville <arnaud.somville@estaca.eu>
2024-12-04 09:52:47 +0100

    Modified doc

--- docs/conf.py
+++ docs/conf.py
@@ -12,55 +12,56 @@ author = 'Your Name'
 release = '0.1'
 
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
+extensions = []
 
-   "sphinx.ext.autodoc",
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-   "sphinx.ext.napoleon",
-
-   "sphinx.ext.viewcode",
-
-   "sphinx.ext.todo",
-
-]
-
-
-templates_path = ["_templates"]
-
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
-
-# Napoleon settings
-
-napoleon_google_docstring = True
-
-napoleon_numpy_docstring = True
-
-napoleon_include_init_with_doc = False
-
-napoleon_include_private_with_doc = False
-
-napoleon_include_special_with_doc = True
 
 
 # -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+html_theme = 'alabaster'
+html_static_path = ['_static']
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../../src"))
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information -----------------------------------------------------
+project = "thermal_solver_1D"
+copyright = "2024, Arnaud SOMVILLE"
+author = "Arnaud SOMVILLE"
+release = "0.0.1"
+
+# -- General configuration ---------------------------------------------------
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.todo",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+
+# -- Options for HTML output -------------------------------------------------
 html_theme = "sphinx_rtd_theme"
-
 html_static_path = ["_static"]
-
 html_theme_options = {
-
-   "page_width": "auto",
-
-   "sidebar_width": "220px",
-
-   "fixed_sidebar": True,
-
-   "show_related": True,  # Ajoute des liens vers les pages connexes.
-
+    "page_width": "auto",
+    "sidebar_width": "220px",
+    "fixed_sidebar": True,
+    "show_related": True,  # Ajoute des liens vers les pages connexes.
 }
-
-
-
